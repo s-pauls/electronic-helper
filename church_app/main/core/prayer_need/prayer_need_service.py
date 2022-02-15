0ff56f1de612280f7bae9c550d0803b5582060f0
Sergey Pauls <s.pauls@ostrean.com>
2022-02-15 10:09:23 +0300

    Логирование. Добавил логировани в PrayerNeedService

--- church_app/main/core/prayer_need/prayer_need_service.py
+++ church_app/main/core/prayer_need/prayer_need_service.py
@@ -1,3 +1,5 @@
+import logging
+
 from .prayer_needs_text_analyzer import PrayerNeedsTextAnalyzer
 from .remote_bot_default_template_parser import RemoteBotDefaultTemplateParser
 from ..notion.notion_service import NotionService
@@ -11,6 +13,7 @@ class PrayerNeedService:
 
     def __init__(self):
         self._notion_service = NotionService()
+        self._logger = logging.getLogger(__name__)
 
     def process_telegram_message(self, telegram_update_object):
 
@@ -19,25 +22,30 @@ class PrayerNeedService:
         if not update.get_message_text():
             return
 
+        message_id = update.get_message_id()
         message_text = update.get_message_text()
         sender_name = update.get_first_name() + " " + update.get_last_name()
 
-        # в этот чат попадает текст пуш-уведомлений из вайбера
         if update.get_chat_id() == TELEGRAM_FROM_VIBER_CHAT_ID:
+            self._logger.debug(f'Message id: {message_id}. '
+                               f'Processing message from Telegram\'s Viber retranslator')
+            # в этот чат попадает текст всех пуш-уведомлений из всех групп Вайбера
             parser = RemoteBotDefaultTemplateParser(message_text)
 
             if parser.get_caption() != TELEGRAM_FROM_VIBER_CHAT_NAME:
+                self._logger.debug(f'Message id: {message_id}. '
+                                   f'Processing interrupted due to this viber group was not expected')
                 return
 
             sender_name = parser.get_sender_name()
             message_text = parser.get_message_text()
 
-            self.process_message(sender_name, message_text, 'viber')
+            self.process_message(sender_name, message_id, message_text, 'viber')
 
         else:
-            self.process_message(sender_name, message_text, 'telegram')
+            self.process_message(sender_name, message_id, message_text, 'telegram')
 
-    def process_message(self, sender_name: str, message_text: str, message_source: str) -> bool:
+    def process_message(self, sender_name: str, message_id: int, message_text: str, message_source: str) -> bool:
 
         analyzer = PrayerNeedsTextAnalyzer(message_text)
 
@@ -46,4 +54,6 @@ class PrayerNeedService:
 
             return True
 
+        self._logger.debug(f'Message id: {message_id}. '
+                           f'Processing interrupted due to message has not prayers need')
         return False
