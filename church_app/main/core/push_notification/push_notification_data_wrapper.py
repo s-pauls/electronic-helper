f9335560a5a0ad2c234fc4aeb30fbab8e9a896f2
Sergey Pauls <s.pauls@ostrean.com>
2022-02-16 23:27:46 +0300

    Исправления. Ошибки в PushNotificationDataWrapper

--- church_app/main/core/push_notification/push_notification_data_wrapper.py
+++ church_app/main/core/push_notification/push_notification_data_wrapper.py
@@ -3,25 +3,25 @@ class PushNotificationDataWrapper:
         self._data: dict = data
 
     def get_app_name(self) -> str:
-        return self._user.get('name')
+        return self._data.get('name')
 
     def get_pkg(self) -> str:
-        return self._user.get('pkg')
+        return self._data.get('pkg')
 
     def get_title(self) -> str:
-        return self._user.get('title')
+        return self._data.get('title')
 
     def get_text(self) -> str:
-        return self._user.get('text')
+        return self._data.get('text')
 
     def get_user_name(self) -> str:
-        return self._user.get('user')
+        return self._data.get('user')
 
     def get_subtext(self) -> str:
-        return self._user.get('subtext')
+        return self._data.get('subtext')
 
     def get_big_text(self) -> str:
-        return self._user.get('bigtext')
+        return self._data.get('bigtext')
 
     def get_info_text(self) -> str:
-        return self._user.get('infotext')
+        return self._data.get('infotext')
